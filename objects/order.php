0a9a33d19b9cd76819184a31217382c64a7fd713
rezatrue <rezatrue@gmail.com>
2019-01-06 08:22:21 +0600

    project submitted to IIT, DU

--- objects/order.php
+++ objects/order.php
@@ -67,7 +67,7 @@ class Order{
 	$query = "SELECT
 					orderID, restaurantSerialNo, userSerialNo, orderDate, orderStatus, deliverPhone, deliverAddress, deliverLat, deliverLng
 				FROM
-					" . $this->table_name . " WHERE restaurantSerialNo = ? AND orderStatus NOT -1 AND 4";
+					" . $this->table_name . " WHERE restaurantSerialNo = ? AND orderStatus NOT IN (-1, 4)";
 	
 	// prepare query statement
 	$stmt = $this->conn->prepare($query);
